2a1c9e9bfdc1e3784be2ce2b0f99fb7236f683c0
Shane Castro <mr.shanecastro@gmail.com>
2024-05-02 19:51:54 -0400

    Initial working app

---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1 className="text-xl bold">
+        Sorry, but we couldnt find that particular snippet
+      </h1>
+    </div>
+  );
+};
+export default NotFoundPage;
